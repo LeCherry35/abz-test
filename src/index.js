f0beda1b9b52f794081728943dfd2aceb2e91e93
Cherry <lenyamm35@gmail.com>
2023-12-06 00:08:19 +0200

    createRoot update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import './index.scss';
 import App from './App';
 import './index.scss'
 
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById('root');
+
+const root = createRoot(container)
+root.render(<App/>)
+
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(<App />, document.getElementById("root"));
 
 const devMode = process.env.NODE_ENV === 'development';
 if (devMode && module && module.hot) {
