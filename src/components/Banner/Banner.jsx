357452b621be7f66032da4c429a93553828ce40a
Cherry <lenyamm35@gmail.com>
2023-12-07 20:28:25 +0200

    banner darkened and image positioned

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,11 +4,13 @@ import Button from '../Button/Button'
 const Banner = () => {
   return (
     <div className='banner-container'>
+      <div className='banner-container__darkener'>
         <div className='banner-content'>
             <h2 className='banner-content__heading'>Test assignment for front-end developer</h2>
             <p className='banner-content__text'>What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.</p>
             <Button name="Sign up"/>
         </div>
+      </div>
     </div>
   )
 }
