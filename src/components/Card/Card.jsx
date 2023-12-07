bf525cacf943c4c1ee2228a3599f3c748f23991d
Cherry <lenyamm35@gmail.com>
2023-12-08 00:52:22 +0200

    card text owerflow fixi

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,12 +3,12 @@ import defaultAvatar from "../../img/photo-cover.svg"
 
 const Card = ({name, email, photo, position, phone}) => {
   return (
-    <div className='cardContainer'>
-        <div className='avatar'>
+    <div className='card-container'>
+        <div className='card-container__avatar'>
             <img src={photo || defaultAvatar} alt="avatar"/>
         </div>
         <p className='text truncatedText' title={name}>{name}</p>
-        <div className='info'>
+        <div className='card-container__info'>
             <p className='text truncatedText' title={position}>{position}</p>
             <p className='text truncatedText' title={email}>{email}</p>
             <p className='text truncatedText' title={phone}>{phone}</p>
