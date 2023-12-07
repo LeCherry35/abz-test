644b8e9735afc2c56c901e8804530c1e98305f0e
Cherry <lenyamm35@gmail.com>
2023-12-07 20:33:11 +0200

    image input refactored

--- src/components/ImageInput/ImageInput.jsx
+++ src/components/ImageInput/ImageInput.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 
-const ImageInput = ({onChange, imageName = null}) => {
+const ImageInput = ({onChange, imageName = null, error}) => {
   return (
-    <div className='imageInputContainer'>
-        <label htmlFor='imgInput' className='imageInput'>
-        <div className='imageUploadButton'>
-            Upload
-        </div>
-        <div >
-            {imageName || 'Upload your photo'}
-        </div>
-        </label>
-        <input
-            id='imgInput'
-            type="file"
-            // value={selectedFile}
-            accept=".jpg, .jpeg"
-            onChange={onChange}
-        />
-    </div>
+    <label htmlFor='imgInput' className='image-input-container'>
+      <div className='image-input-container__image-upload-button'>
+          Upload
+      </div>
+      <div className='image-input-container__image-name'>
+          {imageName || 'Upload your photo'}
+      </div>
+      <div  className='image-input-container__warning-text'>{error}</div>
+      <input
+          className='image-input-container__image-input'
+          id='imgInput'
+          type="file"
+          // value={selectedFile}
+          accept=".jpg, .jpeg"
+          onChange={onChange}
+      />
+    </label>
   )
 }
 
