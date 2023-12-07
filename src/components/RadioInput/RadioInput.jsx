60d6dfc9fe28c3b033d31b0d3fea83f684862e6d
Cherry <lenyamm35@gmail.com>
2023-12-07 20:34:12 +0200

    radio input restyled, select logic added

--- src/components/RadioInput/RadioInput.jsx
+++ src/components/RadioInput/RadioInput.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
-const RadioInput = ({options=[]}) => {
+const RadioInput = ({options=[], setSelected}) => {
   return (
-    <div className='radioInputsContainer'>
+    <div className='radio-inputs-container'>
         {options?.map((option, id) => {
           return (
-            <label htmlFor={'input_' + id} className='radioInput' key={option.id}>
-              <input id={'input_' + id} type='radio' value={option.id} />
+            <label htmlFor={'input_' + id} className='radio-inputs-container__radio-label' key={option.id}>
+              <input 
+                className='radio-inputs-container__radio-input' 
+                id={'input_' + id} 
+                type='radio' 
+                value={option.id} 
+                name='radioInputs'
+                onClick={() => setSelected(option.id)}
+              />
               {option.name}
             </label>
           )
