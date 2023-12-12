af5c593779860a03a6e48ead837d86593e9c7b20
Cherry <lenyamm35@gmail.com>
2023-12-12 14:10:06 +0200

    Image input style fix

--- src/components/ImageInput/ImageInput.jsx
+++ src/components/ImageInput/ImageInput.jsx
@@ -14,7 +14,7 @@ const ImageInput = ({onChange, imageName = null, error}) => {
 			<div className={error ? 'image-input-container__image-upload-button image-input-container__image-upload-button_warning' : 'image-input-container__image-upload-button'}>
           Upload
 			</div>
-			<div className={error ?'image-input-container__image-name image-input-container__image-name' :'image-input-container__image-name'}>
+			<div className={error ? 'image-input-container__image-name image-input-container__image-name_warning' :'image-input-container__image-name'}>
 				{imageName || 'Upload your photo'}
 			</div>
 			<div  className='image-input-container__warning-text'>{error}</div>
